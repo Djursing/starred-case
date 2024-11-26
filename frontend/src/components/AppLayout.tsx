b254b7bfc08cae31414b991d24116ec8e1c75e71
Oliver Djursing <Oliverdjursing@gmail.com>
2024-11-26 19:19:23 +0100

    fix(frontend): stop scrool past html element
    This is not a proper fix, as the nested children elements are not
    contained properly within the page.
    
    This fix is only remove the black background present at the bottom when
    scrolling past the html element.
    
    A proper setup of the project and it's structure is required.

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 
 const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <main className="flex flex-2 p-10 mx-auto h-full">
-      <div className="grow p-7">
-        <div className="mx-auto max-w-7xl">{children}</div>
-      </div>
+    <main className="flex flex-2 p-10 mx-auto h-full min-h-screen overflow-auto">
+      <div className="mx-auto max-w-7xl">{children}</div>
     </main>
   );
 };
